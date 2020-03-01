3a10b28b9fec148908414281477bd0b2f029dcf3
Magno Valdetaro <mvaldetaro@gmail.com>
2020-03-01 16:51:51 -0300

    Configura import de images

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import './App.css';
 
+import nerd from './assests/images/nerd.jpg';
+
 export default function App() {
-  return <h1>Hello World! Nhe!</h1>;
+  return (
+    <>
+      <h1>Hello World! Nerd!</h1>
+      <img src={nerd} alt='nerd' style={{ width: '100%' }} />
+    </>
+  );
 }
--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,10 @@ module.exports = {
         test: /\.css$/,
         use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
       },
+      {
+        test: /.*\.(gif|jpe?g|png)$/i,
+        use: { loader: 'file-loader' },
+      },
     ],
   },
 };
