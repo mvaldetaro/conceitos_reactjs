9bfe9f25e2e0f669149341bbb6d2935c648ef036
Magno Valdetaro <mvaldetaro@gmail.com>
2020-03-02 07:16:02 -0300

    Conceitos de imutabilidade

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './App.css';
-
 import nerd from './assests/images/nerd.jpg';
+import TechList from './components/TechList';
 
-export default function App() {
+function App() {
   return (
     <>
       <h1>Hello World! Nerd!</h1>
       <img src={nerd} alt='nerd' style={{ width: '100%' }} />
+      <TechList />
     </>
   );
 }
+
+export default App;
---
+++ src/components/TechList.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+
+class TechList extends Component {
+  state = {
+    newTech: '',
+    techs: ['Node.js', 'ReactJS', 'React Native'],
+  };
+
+  handleInputChange = e => {
+    this.setState({ newTech: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    this.setState({
+      techs: [...this.state.techs, this.state.newTech],
+      newTech: '',
+    });
+  };
+
+  handleDelete = tech => {
+    this.setState({ techs: this.state.techs.filter(t => t != tech) });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <ul>
+          {this.state.techs.map((tech, index) => (
+            <li key={index}>
+              {tech}
+              <button type='button' onClick={() => this.handleDelete(tech)}>
+                Remover
+              </button>
+            </li>
+          ))}
+        </ul>
+        <input type='text' onChange={this.handleInputChange} value={this.state.newTech} />
+        <button type='submit'>Enviar</button>
+      </form>
+    );
+  }
+}
+
+export default TechList;
